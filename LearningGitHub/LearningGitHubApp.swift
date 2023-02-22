60fa37acfbf67458926d83fc06a2c3ffa99b4d90
Bhavik Bhatt <bhattbhavikr340@gmail.com>
2023-02-22 21:51:15 +0530

    Initial Commit

---
+++ LearningGitHub/LearningGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGitHubApp.swift
+//  LearningGitHub
+//
+//  Created by Bhavik Bhatt on 22/02/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
